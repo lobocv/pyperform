c93ed92fd230e66bb0014046dcd592b360dfa929
Calvin <calvinlobo@gmail.com>
2014-11-01 15:24:28 -0400

    added gitignore. Formatted output.

--- __init__.py
+++ __init__.py
@@ -1,15 +1,7 @@
 __author__ = 'Calvin'
 import inspect
 import timeit
-
-
-from inspect import currentframe, getframeinfo
-
-# frameinfo = getframeinfo(currentframe())
-#
-# print frameinfo.filename, frameinfo.lineno
-
-import math
+from math import log10
 
 class PerformanceTest(object):
 
@@ -21,19 +13,22 @@ class PerformanceTest(object):
 
     def __call__(self, func):
         src = inspect.getsource(func)
-        self._func_src = src[src.index('\n'):]
+        self._func_src = src[src.index('\n') + 1:]
         self._func_name = func.__name__
+
+        print '----------------------------------'
+        print 'Starting performance test on code block:'
         print self._func_src
 
+        # Create the function call statment as a string
         call = "{0}(*{1}, **{2})".format(self._func_name, self._func_args, self._func_kwargs)
-        print call
         self._timer = timeit.Timer(stmt=call, setup=self._func_src)
         trials = self._timer.repeat(self._repeat, self._number)
 
         time_avg_seconds = sum(trials) / len(trials)
-        order = math.log10(time_avg_seconds)
 
         # Convert into reasonable time units
+        order = log10(time_avg_seconds)
         if -6 < order < -3:
             time_units = 'us'
             factor = 1000000
@@ -45,23 +40,26 @@ class PerformanceTest(object):
             factor = 1
 
         time_avg = time_avg_seconds * factor
-        print "Average time ({1}): {0:.3f} {1}".format(time_avg, time_units)
+        print "Average time ({1}): {0:.3f} {1} \n".format(time_avg, time_units)
+        print '----------------------------------'
 
         return func
 
-@PerformanceTest(3, 100, l=10)
+
+@PerformanceTest(3, 10000, 1000)
 def mytest(l):
-    out = []
+    out = 0.
     for i in xrange(l):
-        out.append(i*i)
+        out += i
+
     return out
 
-
-@PerformanceTest(3, 100, 1000)
+@PerformanceTest(3, 10000, 1000)
 def mytest2(l):
-    out = 0
-    for i in xrange(l):
-        out += i * i
+    out = 0.
+    for i in range(l):
+        out += i
+
     return out
 
-
+mytest(5)
