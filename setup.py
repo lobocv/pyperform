61731dd4f208b669b091a58c1bdae8751e4cd9dd
lobocv <elephant123>
2014-12-24 16:00:30 -0500

    bump to version 1.5

--- setup.py
+++ setup.py
@@ -7,11 +7,11 @@ setup(
     name='pyperform',
     packages=['pyperform'],  # this must be the same as the name above
     version=__version__,
-    description='A convenient way to performance test functions and compare results.',
+    description='A fast and convenient way to performance test functions and compare results.',
     author='Calvin Lobo',
     author_email='calvinvlobo@gmail.com',
     url='https://github.com/lobocv/pyperform',
-    download_url='https://github.com/lobocv/pyperform/tarball/1.4',
-    keywords=['testing', 'performance', 'comparison', 'convenience', 'logging'],
+    download_url='https://github.com/lobocv/pyperform/tarball/1.5',
+    keywords=['testing', 'performance', 'comparison', 'convenience', 'logging', 'timeit', 'speed'],
     classifiers=[],
 )
