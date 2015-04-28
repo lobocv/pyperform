90224853019b83c3801b263b4db7ed462f748eff
Calvin <calvinlobo@gmail.com>
2015-04-28 19:34:29 -0400

    @timer was not returning function results

--- pyperform/__init__.py
+++ pyperform/__init__.py
@@ -371,9 +371,10 @@ class ComparisonBenchmark(Benchmark):
 def timer(func):
     def _timed_function(*args, **kwargs):
         t1 = time.time()
-        func(*args, **kwargs)
+        result = func(*args, **kwargs)
         t2 = time.time()
         print(func.__name__, ':\t', convert_time_units(t2-t1))
+        return result
 
     _timed_function.__name__ = "_timed_{}".format(func.__name__)
     return _timed_function
