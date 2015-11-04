1e021d03774fc6aed9513e98a7aab67a06503a8e
clobo <clobo@sensoft.ca>
2015-11-04 12:23:33 -0500

    always make the custom level function binding lower case. Added convenience function create_new_level for easy creation of levels that behave similar to default log levels

--- pyperform/customlogger.py
+++ pyperform/customlogger.py
@@ -30,24 +30,37 @@ class CustomLogLevel(object):
 
         # Create function bindings in the logger or if using the root logger, setup the bindings to allow
         # calls to logging.mycustomlevel() much like logging.info(), logging.debug() etc.
-        setattr(self.logger, self.name, _wrapper)
+        setattr(self.logger, self.name.lower(), _wrapper)
         if self.logger_name is None:
-            setattr(logging, self.name, _wrapper)
+            setattr(logging, self.name.lower(), _wrapper)
 
         return customlevel
+
+
+def create_new_level(level, name, logger_name=None):
+    """
+    Quick way to create a custom log level that behaves like the default levels in the logging module.
+    :param level: level number
+    :param name: level name
+    :param logger_name: optional logger name
+    """
+    @CustomLogLevel(level, name, logger_name)
+    def _default_template(logger, msg, *args, **kwargs):
+        return msg, args, kwargs
 
 
 if __name__ == '__main__':
 
     level = logging.INFO-5
-    name = 'cool'
-    logger_name = 'mylogger'
-    # logger_name = None
+    name = 'MYLEVEL'
+    # logger_name = 'mylogger'
+    logger_name = None
 
     @CustomLogLevel(level, name, logger_name=logger_name)
     def myloglevel(logger, msg, *args, **kwargs):
-        return msg, args, kwargs
+        return 'This is a custom level: %s' % msg, args, kwargs
 
+    # create_new_level(level, name, logger_name=logger_name)
 
     logging.basicConfig()
     if logger_name:
@@ -59,9 +72,9 @@ if __name__ == '__main__':
 
     l.setLevel(logging.INFO)
     logger.info('this is a test')
-    logger.cool('this is a test')
+    logger.mylevel('this is a test')
     l.setLevel(level)
     logger.info('this is a test')
-    logger.cool('this is a test')
+    logger.mylevel('this is a test')
 
 
