e68f096cb7cbab2ae26e9aad4e8b0c59e6c50ac9
clobo <clobo@sensoft.ca>
2016-04-26 14:49:14 -0400

    added ability to profile Processes from multiprocess library

--- pyperform/thread.py
+++ pyperform/thread.py
@@ -7,9 +7,14 @@ import os
 import pstats
 import sys
 import threading
+import multiprocessing
 
 Thread = threading.Thread          # Start off using threading.Thread until changed
+Process = multiprocessing.Process
+
 BaseThread = threading.Thread      # Store the Thread class from the threading module before monkey-patching
+BaseProcess = multiprocessing.Process
+
 profiled_thread_enabled = False
 logged_thread_enabled = True
 
@@ -19,13 +24,14 @@ def enable_thread_profiling(profile_dir, exception_callback=None):
     Monkey-patch the threading.Thread class with our own ProfiledThread. Any subsequent imports of threading.Thread
     will reference ProfiledThread instead.
     """
-    global profiled_thread_enabled, Thread
+    global profiled_thread_enabled, Thread, Process
     if os.path.isdir(profile_dir):
-        ProfiledThread.profile_dir = profile_dir
+        _Profiler.profile_dir = profile_dir
     else:
         raise OSError('%s does not exist' % profile_dir)
-    ProfiledThread.exception_callback = exception_callback
+    _Profiler.exception_callback = exception_callback
     Thread = threading.Thread = ProfiledThread
+    Process = multiprocessing.Process = ProfiledProcess
     profiled_thread_enabled = True
 
 
@@ -40,34 +46,38 @@ def enable_thread_logging(exception_callback=None):
     logged_thread_enabled = True
 
 
-class ProfiledThread(BaseThread):
+class _Profiler(object):
     """
     A Thread that contains it's own profiler. When the SSI_App closes, all profiles are combined and printed
     to a single .profile.
     """
     profile_dir = None
     exception_callback = None
+    _type = '_Profiler'
 
     def run(self):
         profiler = cProfile.Profile()
         try:
-            logging.debug('ProfiledThread: Starting ProfiledThread {}'.format(self.name))
-            profiler.runcall(BaseThread.run, self)
+            logging.debug('{cls}: Starting {cls}: {name}'.format(cls=self._type, name=self.name))
+            profiler.runcall(super(_Profiler, self).run)
+            logging.debug('{cls}: Prepating to exit {cls}: {name}'.format(cls=self._type, name=self.name))
         except Exception as e:
-            logging.error('ProfiledThread: Error encountered in Thread {name}'.format(name=self.name))
+            logging.error('{cls}: Error encountered in {name}'.format(cls=self._type, name=self.name))
             logging.error(e)
-            if ProfiledThread.exception_callback:
+            if self.exception_callback:
                 e_type, e_value, last_traceback = sys.exc_info()
-                ProfiledThread.exception_callback(e_type, e_value, last_traceback)
+                self.exception_callback(e_type, e_value, last_traceback)
         finally:
-            if ProfiledThread.profile_dir is None:
-                logging.warning('ProfiledThread: profile_dir is not specified. '
-                                'Profile \'{}\' will not be saved.'.format(self.name))
+            if self.profile_dir is None:
+                logging.warning('{cls}: profile_dir is not specified. '
+                                'Profile \'{name}\' will not be saved.'.format(cls=self._type, name=self.name))
                 return
             self.print_stats(profiler)
 
     def print_stats(self, profiler):
-        filename = os.path.join(self.profile_dir, self.name)
+        name = (self._type + '-' + self.name) if self._type else self.name
+        filename = os.path.join(self.profile_dir, name)
+        logging.debug('Printing stats for {name}'.format(name=name))
         s = StringIO.StringIO()
         sortby = 'cumulative'
         ps = pstats.Stats(profiler, stream=s)
@@ -102,6 +112,24 @@ class ProfiledThread(BaseThread):
             ps.sort_stats(sortby)
             ps.print_stats()
             f.write(s.getvalue())
+
+
+class ProfiledThread(_Profiler, BaseThread):
+    n_threads = 0
+    _type = 'Thread'
+
+    def __init__(self, *args, **kwargs):
+        super(ProfiledThread, self).__init__(*args, **kwargs)
+        ProfiledThread.n_threads += 1
+
+
+class ProfiledProcess(_Profiler, Process):
+    n_processes = 0
+    _type = 'Process'
+
+    def __init__(self, *args, **kwargs):
+        super(ProfiledProcess, self).__init__(*args, **kwargs)
+        ProfiledProcess.n_processes += 1
 
 
 class LoggedThread(BaseThread):
